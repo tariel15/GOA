baac3d94b607c6e2d26737148397fda65e024b4f
tariel15 <tariel.tata123@gmail.com>
2025-01-13 22:40:22 +0400

    second commit

--- level 01/homework/hw.py
+++ level 01/homework/hw.py
@@ -1,77 +1,15 @@
-from turtle import *
-shape("turtle")
-speed(30)
-width(6)
-color("red")
-forward(200)
-left(90)
+name="tariel"
 
-forward(200)
-left(90)
+surname="bejanishvili"
 
-forward(200)
-left(90)
+age=15
 
-forward(200)
-left(90)
+hight=172.5
 
-forward(70)
-color("green")
-left(90)
-forward(120)
-right(90)
-forward(60)
-right(90)
-forward(120)
+knows_programing=True
 
-penup()
-goto(200,200)
-pendown()
-
-color("red")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-
-color("green")
-penup()
-goto(20,160)
-pendown()
-left(30)
-forward(50)
-left(90)
-forward(30)
-left(90)
-forward(50)
-left(90)
-forward(30)
-
-
-penup()
-goto(180,160)
-pendown()
-left(90)
-forward(50)
-right(90)
-forward(30)
-right(90)
-forward(50)
-right(90)
-forward(30)
+is_ugly=False
+print(name+" "+surname+" "+str(age)+" "+str(hight))
 
 
 
-
-
-
-
-
-
-
-
-
-
-exitonclick()
