6b03a90733077ff53ff3409901fe5ba2752b623d
tariel15 <tariel.tata123@gmail.com>
2025-03-21 14:56:21 +0400

    commit

---
+++ level12/classwork/task1.py
@@ -0,0 +1,8 @@
+x=15
+y=50
+print(x>y)#Fales
+print(x<y)#True
+print(x==y)#Fales
+print(x<=y)#True
+print(x>=y)#Fales
+print(x!=y)#True
---
+++ level12/classwork/task2.py
@@ -0,0 +1,15 @@
+#2) სამივე ლოგიკურ ოპერატორზე: not, or, and: ჩამოწერეთ ყველა შესაძლო ვარიანტი და დაბეჭდეთ შედეგი გვერდზე კომენტარის საშვალებით მიწუერეთ შედეგი
+
+# not operators
+print(not True) #Fales
+print(not False) # True
+
+#or operators
+print(True or False)# True
+print(True or True)# Fales
+print(False or False)# Fales
+
+# and operators
+print(True and True) # True
+print(True and False) # Fales
+print(False and False)# Fales
