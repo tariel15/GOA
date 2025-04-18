e34b59c195748dc8875bc9ceaa7a0451755d840e
tariel15 <tariel.tata123@gmail.com>
2025-04-18 23:37:24 +0400

    commit

--- level20/homework/task2.py
+++ level20/homework/task2.py
@@ -1,11 +1,24 @@
 #5) მომხმარებელს 3 მცდელობა აქვს სწორი PIN კოდის შეყვანისთვის. თუ შეიყვანს სწორად, დაიბეჭდება "Access Granted", სხვა შემთხვევაში "Access Denied" გამოიყენეთ პირობითი განცხადებები
 
-enter_pin_code=""
+enter_pin_code=input("enter pin code")
 pin_code="2009"
 atempt=0
-while enter_pin_code!=pin_code:
-    enter_pin_code=input("enter pin code")
-    atempt+=3
-    if enter_pin_code !=pin_code:
-        print("acces denied")
-else:print("access greanted")
+# while enter_pin_code !=pin_code:
+#     if enter_pin_code ==pin_code:
+#         break
+#     enter_pin_code=input("enter pin code")
+#     atempt +=1
+#     if atempt ==2:
+#         print("acces denied")
+#         break
+while atempt !=3:
+    if enter_pin_code != pin_code:
+        if atempt ==2:
+            print("acess denied")
+            break
+        else:
+            enter_pin_code=input("enter pin code")
+            atempt +=1
+    elif enter_pin_code ==pin_code:
+        print("acess granted")
+        atempt =3
