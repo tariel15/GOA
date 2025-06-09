86493d5a0844d0e44ebee0f24aa1407bfe1e5f12
tariel15 <tariel.tata123@gmail.com>
2025-06-09 23:43:59 +0400

    commit

---
+++ level34/classwork/codwars2.py
@@ -0,0 +1,7 @@
+#https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+# Even or Odd
+def even_or_odd(number):
+    if number %2==0:
+        return "Even"
+    else:
+        return "Odd"
---
+++ level34/homework/task1.py
@@ -0,0 +1,9 @@
+#https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+#https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+#https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+#https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+
+#https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/python
